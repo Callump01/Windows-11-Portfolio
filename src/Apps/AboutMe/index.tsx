2690bc41f3fe1d00bf2ca6999976cd7b8fb8babd
Callum <Callump01@users.noreply.github.com>
2022-01-02 04:21:56 +0000

    Update index.tsx

--- src/Apps/AboutMe/index.tsx
+++ src/Apps/AboutMe/index.tsx
@@ -1,15 +1,14 @@
 export const AboutMeApp = () => {
   return (
-<div style="padding: 10px">
+<div style="padding: 10px;">
 <p style="text-align: right;"><img style="display: block; margin-left: auto; margin-right: auto;" src="https://i.imgur.com/1iWsYGt.png" alt="" width="250" height="250" /></p>
 <h4>&nbsp;</h4>
-<h2 style="text-align: center;">About Me</h2>
-<p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;">Hi there, I'm Callum.</p>
+<h2 style="text-align: center;">Callum Pope</h2>
+<p style="text-align: center;">Snr. IAM Consultant &amp; Security Researcher</p>
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;">I'm a confident, reliable and multi-skilled Identity consultant with comprehensive understanding and experience in designing, developing, and deploying Identity and Access Management (IAM) infrastructure, alongside maintaining and triaging existing client infrastructure through years of experience delivering successful identity solutions for both multinational enterprises and small-medium business solutions.</p>
 <p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;">I'm an excellent cross-functional collaborator with an ability to multi-task well under pressure and relate with stakeholders of all seniorities and technical ability, and have a curious personality with a positive mindset and a dedication to continued growth.</p>
+<p style="text-align: center;">I'm an excellent cross-functional collaborator with an ability to multi-task well under pressure and relate with stakeholders of all seniorities and technical ability and have a curious personality with a positive mindset and dedication to continued growth.</p>
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;"><strong>I am security cleared to a high government standard.</strong></p>
 <p style="text-align: center;">&nbsp;</p>
