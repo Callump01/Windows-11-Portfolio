1a791a8ff05383f558a6a93c8c6962a6c302d514
Callum <Callump01@users.noreply.github.com>
2022-01-02 02:41:28 +0000

    Update index.tsx

--- src/Apps/MIM/index.tsx
+++ src/Apps/MIM/index.tsx
@@ -1,21 +1,9 @@
 export const MimApp = () => {
   return (
-	<iframe
-	  id="AboutMe"
-      src="about:blank"
-      title="About Me"
-      width="100%"
-      height="100%"
-      style={{
-        borderRadius: "0 0 6px 6px",
-        border: "none",
-      }}
-    />
-	<script type="text/javascript">
-   var doc = document.getElementById('AboutMe').contentWindow.document;
-   doc.open();
-   doc.write('<h4>About Me</h4> <p>Hi there, I\'m Callum.</p> <p>I\'m a confident, reliable and multi-skilled Identity consultant with comprehensive understanding and experience in designing, developing, and deploying Identity and Access Management (IAM) infrastructure, alongside maintaining and triaging existing client infrastructure through years of experience delivering successful identity solutions for both multinational enterprises and small-medium business solutions. </p> <p>I\'m an excellent cross-functional collaborator with an ability to multi-task well under pressure and relate with stakeholders of all seniorities and technical ability, and have a curious personality with a positive mindset and a dedication to continued growth. </p> <p><b>I am security cleared to a high government standard.</b></p>');
-   doc.close();
-</script>
+<h4>About Me</h4>
+  <p>Hi there, I\'m Callum.</p>
+  <p>I\'m a confident, reliable and multi-skilled Identity consultant with comprehensive understanding and experience in designing, developing, and deploying Identity and Access Management (IAM) infrastructure, alongside maintaining and triaging existing client infrastructure through years of experience delivering successful identity solutions for both multinational enterprises and small-medium business solutions.</p>
+  <p>I\'m an excellent cross-functional collaborator with an ability to multi-task well under pressure and relate with stakeholders of all seniorities and technical ability, and have a curious personality with a positive mindset and a dedication to continued growth.</p>
+  <p><b>I am security cleared to a high government standard.</b></p>
   );
 };
