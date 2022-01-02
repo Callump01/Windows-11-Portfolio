b7b4008963d2471537d55fbb249fcec6c9cb172c
Callum <Callump01@users.noreply.github.com>
2022-01-02 02:32:39 +0000

    About Me section amendments

--- src/Apps/MIM/index.tsx
+++ src/Apps/MIM/index.tsx
@@ -1,21 +1,14 @@
 export const MimApp = () => {
   return (
-  <h4>About Me</h4>
-  <p>Hi there, I'm Callum.</p>
-  <p>I'm a confident, reliable and multi-skilled Identity consultant with comprehensive understanding and experience in designing, developing, and deploying Identity and Access Management (IAM) infrastructure, alongside maintaining and triaging existing client infrastructure through years of experience delivering successful identity solutions for both multinational enterprises and small-medium business solutions.
-  </p>
-  <p>I'm an excellent cross-functional collaborator with an ability to multi-task well under pressure and relate with stakeholders of all seniorities and technical ability, and have a curious personality with a positive mindset and a dedication to continued growth.
-  </p>
-  <p><b>I am security cleared to a high government standard.</b></p>
-    /* <iframe
-      src="https://github1s.com/PiyushSuthar/Windows-11-Web"
-      title="Windows 11 Web"
+	<iframe
+      src="https://portfolio-19qa9270g-callump01.vercel.app/src/Views/Apps/AboutMe.html"
+      title="About Me"
       width="100%"
       height="100%"
       style={{
         borderRadius: "0 0 6px 6px",
         border: "none",
       }}
-    />*/
+    />
   );
 };
