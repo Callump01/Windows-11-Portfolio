dadc95fe7d12080fee122bdba13d8e92ce777089
Callum <Callump01@users.noreply.github.com>
2025-11-18 05:29:30 +0000

    Update job title and contact link text

--- src/Apps/AboutMe/index.tsx
+++ src/Apps/AboutMe/index.tsx
@@ -9,14 +9,14 @@ export const AboutMeApp = () => {
 <p style="text-align: right;"><img style="display: block; margin-left: auto; margin-right: auto;" src={selfie_img} alt="An portrait image of Callum Pope" width="250" height="250" /></p>
 <h4>&nbsp;</h4>
 <h2 style="text-align: center;">Callum Pope</h2>
-<p style="text-align: center;">Snr. Identity Consultant &amp; Cybersecurity Professional</p>
+<p style="text-align: center;">Cybersecurity Consultant &amp; IdAM Specialist</p>
 <p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;">I am a seasoned Identity and Access Management (IdAM) consultant, offering a wealth of expertise in designing, developing, and deploying identity infrastructure.</p>
+<p style="text-align: center;">I am a seasoned Identity and Access Management (IdAM) consultant, offering a wealth of expertise in designing, developing and deploying identity infrastructure.</p>
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;">I'm comfortable liaising with stakeholders at all levels with a proven track record of consistently delivering successful identity solutions tailored to meet unique and ever-evolving business challenges.</p>
 <p style="text-align: center;">&nbsp;</p>
 <p style="text-align: center;">&nbsp;</p>
-<p style="text-align: center;"><a style="text-align: centerfont-size: 18px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-weight: bold; text-decoration: none; border-radius: 5px; background-color: #193f7d; border-top: 12px solid #193f7d; border-bottom: 12px solid #193f7d; border-right: 18px solid #193f7d; border-left: 18px solid #193f7d; display: inline-block;" href="mailto:contact@callumpope.com" target="_blank">Contact Me</a></p>
+<p style="text-align: center;"><a style="text-align: centerfont-size: 18px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-weight: bold; text-decoration: none; border-radius: 5px; background-color: #193f7d; border-top: 12px solid #193f7d; border-bottom: 12px solid #193f7d; border-right: 18px solid #193f7d; border-left: 18px solid #193f7d; display: inline-block;" href="mailto:contact@callumpope.com" target="_blank">Get in touch</a></p>
 
 </div>
   );
